33b269cbda23e5516f43e1aec0dc7348b4b08512
danil <danil@DESKTOP-P63D37J>
2024-10-24 23:44:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace RemoveSpecialChars
+{
+    public class Program
+    {
+        public static string RemoveSpecialChars(string input, char[] targets)
+        {
+            string[] subStrings = input.Split(targets);
+            string output = "";
+            foreach (string subString in subStrings)
+            {
+                output = String.Concat(output, subString);
+            }
+            return output;
+        }
+        public static void Main(string[] args)
+        {
+            string input = "this is a!@ test. Remove # special % chars";
+            char[] targets = { '!', '@', '#', '%', '.' };
+
+            string result = RemoveSpecialChars(input, targets);
+            Console.WriteLine("Original string: " + input);
+            Console.WriteLine("Processed string: " + result);
+            Console.WriteLine("Press 'Enter' to over");
+            Console.Read();
+        }
+    }
+}
